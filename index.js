8e000c532bc163ac34cff8134fdfd60647a0f90e
hennycraft52 <122104659+Hennycraft52@users.noreply.github.com>
2023-10-21 22:12:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,6 @@
-class GiraHomeServerPlatform {
-  constructor(log, config, api) {
-    this.log = log;
-    this.config = config;
-    this.api = api;
+const GiraHomeServerPlatform = require('./GiraHomeserverPlatform');
 
-    // Füge die Konfigurationselemente für die Homebridge-UI hinzu
-    this.configureAccessory();
-  }
+module.exports = (homebridge) => {
+  homebridge.registerPlatform('homebridge-gira-homeserver', 'GiraHomeServer', GiraHomeServerPlatform);
+};
 
-  configureAccessory() {
-    const accessory = new this.api.platformAccessory('Gira HomeServer Configuration', 'configAccessory');
-    const service = accessory.addService(this.api.hap.Service.Switch, 'Gira HomeServer Switch');
-    
-    // Füge eine Characteristic hinzu, um die Konfiguration zu steuern
-    service.addCharacteristic(this.api.hap.Characteristic.On)
-      .on('get', this.getConfiguration.bind(this))
-      .on('set', this.setConfiguration.bind(this));
-
-    // Füge das Accessory zur Plattform hinzu
-    this.api.registerPlatformAccessories('homebridge-gira-homeserver', 'GiraHomeServer', [accessory]);
-  }
-
-  getConfiguration(callback) {
-    // Hier kannst du die aktuelle Konfiguration auslesen
-    // und über die Homebridge-UI anzeigen
-    const currentConfiguration = ...; // Aktuelle Konfiguration abrufen
-    callback(null, currentConfiguration);
-  }
-
-  setConfiguration(value, callback) {
-    // Hier kannst du die Konfiguration basierend auf dem in der Homebridge-UI
-    // festgelegten Wert aktualisieren
-    // und die Änderungen in der `config.json` speichern
-    const newConfiguration = value; // Neuen Konfigurationswert erhalten
-    // Speichere die Konfigurationsänderungen
-    callback(null);
-  }
-}
-
-module.exports = GiraHomeServerPlatform;
