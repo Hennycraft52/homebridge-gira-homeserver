449f8c038d741523e236050c6b0ee0f0c0768915
hennycraft52 <122104659+Hennycraft52@users.noreply.github.com>
2023-10-21 22:11:47 +0200

    Update GiraHomeServerPlatform.js

--- GiraHomeServerPlatform.js
+++ GiraHomeServerPlatform.js
@@ -1,67 +1,42 @@
-const GiraHomeServer = require('gira-homeserver-library'); // Fügen Sie Ihre Gira HomeServer Library hinzu
-
 class GiraHomeServerPlatform {
   constructor(log, config, api) {
     this.log = log;
     this.config = config;
     this.api = api;
 
-    this.homebridgeAccessories = [];
-    this.giraHomeServer = new GiraHomeServer({
-      host: this.config.host, // Die IP-Adresse oder Hostname Ihres Gira HomeServers
-      username: this.config.username, // Ihr Benutzername
-      password: this.config.password, // Ihr Passwort
-    });
-
-    this.discoverDevices();
+    // Füge die Konfigurationselemente für die Homebridge-UI hinzu
+    this.configureAccessory();
   }
 
-  configureAccessory(accessory) {
-    // Hier können Sie vorhandene Accessories verarbeiten, wenn nötig
-    this.log(`Configured Accessory: ${accessory.displayName}`);
+  configureAccessory() {
+    const accessory = new this.api.platformAccessory('My Configuration', 'configAccessory');
+    const service = accessory.addService(this.api.hap.Service.Switch, 'My Switch');
+    
+    // Füge eine Characteristic hinzu, um die Konfiguration zu steuern
+    service.addCharacteristic(this.api.hap.Characteristic.On)
+      .on('get', this.getConfiguration.bind(this))
+      .on('set', this.setConfiguration.bind(this));
+
+    // Füge das Accessory zur Plattform hinzu
+    this.api.registerPlatformAccessories('homebridge-gira-homeserver', 'GiraHomeServer', [accessory]);
   }
 
-  discoverDevices() {
-    // Hier können Sie Ihre Gira-Geräte entdecken und Accessories hinzufügen
-    // Verwenden Sie this.addAccessory(), um Accessories hinzuzufügen
-    // Beispiel:
-    // this.addAccessory('Licht', 'LightSwitch', 'Light Switch', 'Switch');
+  getConfiguration(callback) {
+    // Hier kannst du die aktuelle Konfiguration auslesen
+    // und über die Homebridge-UI anzeigen
+    const currentConfiguration = ...; // Aktuelle Konfiguration abrufen
+    callback(null, currentConfiguration);
   }
 
-  addAccessory(deviceName, uuid, displayName, category) {
-    // Hier können Sie ein neues HomeKit-Accessory erstellen und hinzufügen
-    const accessory = new this.api.platformAccessory(displayName, uuid);
-
-    accessory.category = this.api.hap.Categories[category];
-    this.log(`Adding Accessory: ${displayName}`);
-
-    // Hier können Sie die Konfiguration für das Accessory definieren
-    // Beispielsweise Service- und Characteristic-Definitionen
-
-    // Beispiel:
-    // const switchService = accessory.addService(this.api.hap.Service.Switch, 'Light Switch');
-    // const onCharacteristic = switchService.getCharacteristic(this.api.hap.Characteristic.On);
-
-    // Fügen Sie Ihre Logik hinzu, um das Gerät zu steuern
-
-    // Beispiel:
-    // onCharacteristic.on('get', this.getSwitchState.bind(this));
-    // onCharacteristic.on('set', this.setSwitchState.bind(this));
-
-    this.homebridgeAccessories.push(accessory);
+  setConfiguration(value, callback) {
+    // Hier kannst du die Konfiguration basierend auf dem in der Homebridge-UI
+    // festgelegten Wert aktualisieren
+    // und die Änderungen in der `config.json` speichern
+    const newConfiguration = value;
+    // Speichere die Konfigurationsänderungen
+    callback(null);
   }
-
-  // Hier können Sie Methoden zum Steuern Ihrer Accessories implementieren
-  // Beispiel:
-  // getSwitchState(callback) {
-  //   // Logik, um den Status des Schalters abzurufen
-  // }
-
-  // setSwitchState(value, callback) {
-  //   // Logik, um den Schalterstatus festzulegen
-  // }
 }
 
-module.exports = (api) => {
-  api.registerPlatform('GiraHomeServerPlatform', 'GiraHomeServer', GiraHomeServerPlatform);
-};
+module.exports = GiraHomeServerPlatform;
+
