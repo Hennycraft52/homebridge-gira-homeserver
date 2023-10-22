35029b16b9638828bd78719b55e066897099882d
hennycraft52 <122104659+Hennycraft52@users.noreply.github.com>
2023-10-22 17:39:07 +0200

    Update GiraHomeserverConnect.js

--- GiraHomeserverConnect.js
+++ GiraHomeserverConnect.js
@@ -1,15 +1,31 @@
 const axios = require('axios');
 
-module.exports = {
-  connect: async (host, log) => {
-    log('Connecting to Gira HomeServer...');
+class GiraHomeserverConnect {
+  constructor(log, host) {
+    this.log = log;
+    this.host = host;
+  }
+
+  // Funktion zum Abrufen eines Datenpunkts
+  async getDatenpunkt(urn) {
     try {
-      const response = await axios.get(`http://${host}/api/someEndpoint`);
-      log('Connected to Gira HomeServer successfully');
-      return response;
+      const response = await axios.get(`http://${this.host}/api/${urn}`);
+      return response.data.value; // Wert des Datenpunkts
     } catch (error) {
-      log.error(`Error while connecting to Gira HomeServer: ${error}`);
-      throw error;
+      this.log.error('Fehler beim Abrufen des Datenpunkts:', error);
+      return null;
     }
-  },
-};
+  }
+
+  // Funktion zum Aktualisieren eines Datenpunkts
+  async setDatenpunkt(urn, value) {
+    try {
+      await axios.put(`http://${this.host}/api/${urn}`, { value });
+      this.log(`Datenpunkt ${urn} wurde auf ${value} gesetzt.`);
+    } catch (error) {
+      this.log.error('Fehler beim Aktualisieren des Datenpunkts:', error);
+    }
+  }
+}
+
+module.exports = GiraHomeserverConnect;
