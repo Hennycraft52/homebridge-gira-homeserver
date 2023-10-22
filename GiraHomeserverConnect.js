e955de57e89d01bda5750e40b0df67584172a971
hennycraft52 <122104659+Hennycraft52@users.noreply.github.com>
2023-10-22 17:08:37 +0200

    Update GiraHomeserverConnect.js

--- GiraHomeserverConnect.js
+++ GiraHomeserverConnect.js
@@ -1,24 +1,15 @@
 const axios = require('axios');
 
-class GiraHomeserverConnect {
-  constructor(log, host, api) {
-    this.log = log;
-    this.host = host;
-    this.api = api;
-
-    // Implement your logic for interacting with the Gira IoT REST API here
-    // Use Axios or another HTTP library to make API requests
-    // For example:
-    axios.get(`http://${this.host}/api/someEndpoint`)
-      .then(response => {
-        // Process the API response and create Homebridge devices
-        // Use this.log to log information
-        // Create devices using this.api.registerPlatformAccessories
-      })
-      .catch(error => {
-        this.log.error(`Error while connecting to Gira HomeServer: ${error}`);
-      });
-  }
-}
-
-module.exports = GiraHomeserverConnect;
+module.exports = {
+  connect: async (host, log) => {
+    log('Connecting to Gira HomeServer...');
+    try {
+      const response = await axios.get(`http://${host}/api/someEndpoint`);
+      log('Connected to Gira HomeServer successfully');
+      return response;
+    } catch (error) {
+      log.error(`Error while connecting to Gira HomeServer: ${error}`);
+      throw error;
+    }
+  },
+};
