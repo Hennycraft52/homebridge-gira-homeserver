fbc7ee4fd83e5ebcf0743717b4d58e89b681dfed
hennycraft52 <122104659+Hennycraft52@users.noreply.github.com>
2023-10-22 09:18:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
-const { API } = require('homebridge');
+const Homebridge = require('homebridge');
+const GiraHomeServer = require('gira-home-server-library'); // Dies ist ein fiktives Beispiel. Du musst die richtige Bibliothek verwenden.
 
-class GiraHomeserverPlatform {
+class GiraHomeServerPlatform {
   constructor(log, config, api) {
     this.log = log;
     this.config = config;
     this.api = api;
 
-    // Hier implementierst du den restlichen Code für deine Plattform
+    this.homebridgeAccessories = [];
+
+    // Verbinde dich mit dem Gira HomeServer
+    this.giraHomeServer = new GiraHomeServer({
+      host: this.config.host,
+      username: this.config.username,
+      password: this.config.password,
+    });
+
+    this.giraHomeServer.on('deviceDiscovered', this.handleDeviceDiscovery.bind(this));
+
+    // Registriere die Plattform beim Homebridge
+    this.api.registerPlatform('GiraHomeServer', 'GiraHomeServer', this);
   }
 
-  // Weitere Methoden und Logik für deine Plattform
+  configureAccessory(accessory) {
+    // Konfiguriere vorhandene Accessories, wenn nötig
+  }
+
+  handleDeviceDiscovery(device) {
+    // Hier kannst du ein Homebridge-Accessory für das gefundene Gerät erstellen
+    const accessory = new Homebridge.platformAccessory(device.name, Homebridge.hap.uuid.generate(device.id));
+    
+    // Konfiguriere das Accessory
+    // Zum Beispiel: accessory.addService(Homebridge.hap.Service.Switch, 'Schalter', 'SchalterUUID');
+
+    // Füge das Accessory zur Plattform hinzu
+    this.api.registerPlatformAccessories('GiraHomeServer', 'GiraHomeServer', [accessory]);
+
+    this.homebridgeAccessories.push(accessory);
+  }
+
+  accessories(callback) {
+    // Gib die konfigurierten Accessories zurück
+    callback(this.homebridgeAccessories);
+  }
 }
 
 module.exports = (api) => {
-  api.registerPlatform('GiraHomeServer', 'GiraHomeserverPlatform', GiraHomeserverPlatform);
+  Homebridge = api.hap;
+  api.registerPlatform('GiraHomeServer', 'GiraHomeServer', GiraHomeServerPlatform);
 };
